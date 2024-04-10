8933a96760a568d68087f1cb1afcffe8a453cab8
Niyoj Oli <oliniyoj@gmail.com>
2024-04-10 22:34:54 +0545

    adds lualine

--- init.lua
+++ init.lua
@@ -22,45 +22,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Tuples for lazy config
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { "nvim-telescope/telescope.nvim", name = "telescope", dependencies = "tsakirist/telescope-lazy.nvim" },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    },
-    {
-        "fdschmidt93/telescope-egrepify.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function () 
-          local configs = require("nvim-treesitter.configs")
-    
-          configs.setup({
-              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },  
-            })
-        end
-     },
-     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-          "MunifTanjim/nui.nvim",
-        }
-    }
-}
 
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins", opts)
 
 vim.keymap.set("n", "<C-n>", ":Neotree")
 
---
+++ lua/plugins.lua
@@ -0,0 +1,35 @@
+return {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", name = "telescope", dependencies = "tsakirist/telescope-lazy.nvim" },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "fdschmidt93/telescope-egrepify.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+          local configs = require("nvim-treesitter.configs")
+    
+          configs.setup({
+              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+              sync_install = false,
+              highlight = { enable = true },
+              indent = { enable = true },  
+            })
+        end
+     },
+     {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+        }
+    }
+}
