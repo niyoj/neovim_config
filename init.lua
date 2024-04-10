e16769844dcedb75d020118b2dd5f727067d44f7
Niyoj Oli <oliniyoj@gmail.com>
2024-04-10 22:23:49 +0545

    adds neotree

--- init.lua
+++ init.lua
@@ -46,12 +46,23 @@ local plugins = {
               indent = { enable = true },  
             })
         end
-     }
+     },
+     {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+        }
+    }
 }
 
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+vim.keymap.set("n", "<C-n>", ":Neotree")
 
 -- See this for treesitter on Windows; https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support
 require 'nvim-treesitter.install'.prefer_git = false
