74b85f75fd362c98a3747514ff77698d8cb08663
Niyoj Oli <oliniyoj@gmail.com>
2024-04-10 22:08:57 +0545

    adds treesitter telescope

--- init.lua
+++ init.lua
@@ -22,7 +22,44 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Tuples for lazy config
-local plugins = {}
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-telescope/telescope.nvim", name = "telescope", dependencies = "tsakirist/telescope-lazy.nvim" },
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "fdschmidt93/telescope-egrepify.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+          local configs = require("nvim-treesitter.configs")
+    
+          configs.setup({
+              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+              sync_install = false,
+              highlight = { enable = true },
+              indent = { enable = true },  
+            })
+        end
+     }
+}
+
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+-- See this for treesitter on Windows; https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support
+require 'nvim-treesitter.install'.prefer_git = false
+require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
+
+require("telescope").load_extension "lazy"
+require("telescope").load_extension "file_browser"
+require "telescope".load_extension "egrepify"
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
