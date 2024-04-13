e56833695aa33ca7ed73cea6729da2e0d979bf48
Niyoj Oli <oliniyoj@gmail.com>
2024-04-13 09:31:35 +0545

    adds autoclose on neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,62 +1,71 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-          event = "neo_tree_buffer_leave",
-          handler = function()
-            local shown_buffers = {}
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              shown_buffers[vim.api.nvim_win_get_buf(win)] = true
-            end
-            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-              if
-                  not shown_buffers[buf]
-                  and vim.api.nvim_buf_get_option(buf, "buftype") == "nofile"
-                  and vim.api.nvim_buf_get_option(buf, "filetype") == "neo-tree"
-              then
-                vim.api.nvim_buf_delete(buf, {})
-              end
-            end
-          end,
-        },
-      },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+				{
+					event = "neo_tree_buffer_leave",
+					handler = function()
+						local shown_buffers = {}
+						for _, win in ipairs(vim.api.nvim_list_wins()) do
+							shown_buffers[vim.api.nvim_win_get_buf(win)] = true
+						end
+						for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+							if
+								not shown_buffers[buf]
+								and vim.api.nvim_buf_get_option(buf, "buftype") == "nofile"
+								and vim.api.nvim_buf_get_option(buf, "filetype") == "neo-tree"
+							then
+								vim.api.nvim_buf_delete(buf, {})
+							end
+						end
+					end,
+				},
+			},
       close_if_last_window = true,
-      default_component_configs = {
-        git_status = {
-          symbols = {
-            -- Change type
-            added = "A", -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted = "✖", -- this can only be used in the git_status source
-            renamed = "R", -- this can only be used in the git_status source
-            -- Status type
-            untracked = "U",
-            ignored = "I",
-            unstaged = "",
-            staged = "A",
-            conflict = "",
-          },
-        },
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_hidden = false,
-          never_show = {
-            ".git",
-            ".node_modules",
-          },
-        },
-      },
-    })
-  end,
+			default_component_configs = {
+				git_status = {
+					symbols = {
+						-- Change type
+						added = "A", -- or "✚", but this is redundant info if you use git_status_colors on the name
+						modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+						deleted = "✖", -- this can only be used in the git_status source
+						renamed = "R", -- this can only be used in the git_status source
+						-- Status type
+						untracked = "U",
+						ignored = "I",
+						unstaged = "",
+						staged = "A",
+						conflict = "",
+					},
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_hidden = false,
+					never_show = {
+						".git",
+						".node_modules",
+					},
+				},
+			},
+		})
+	end,
 }
