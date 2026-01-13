e26c54bfc5d77b060707d271f686ec7b4994186e
aspire <niyoj.077bct029@gmail.com>
2026-01-13 12:10:43 +0545

    fixes issue with treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,15 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = { "python", "lua", "vim", "vimdoc", "query", "bash", "json", "yaml" },
-      auto_install = true,
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
+    event = "BufReadPost",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python", "lua", "vim", "vimdoc", "query", "bash", "json", "yaml" },
+        auto_install = true,
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
   },
 }
